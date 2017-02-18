9918398ce8d0d22610743b5f36719cfb4b018f99
Barasbi <betokov93@gmail.com>
2017-02-18 18:22:25 +0300

    add 1

--- includes/zakaz.php
+++ includes/zakaz.php
@@ -5,7 +5,7 @@
 				<h2 class="db-zakaz-h2">КАК ЗАКАЗАТЬ <span>НАБОР?</span></h2>
 			</div>
 			<div class="col-md-3">
-				<img src="img/doc.png" alt="img">
+				<div class="db-zakaz-img"><img src="img/doc.png" alt="img" ></div>
 				<p class="db-zakaz-p"><span>ЗАЯВКА НА САЙТЕ</span><br>
 					Вы оставляете <br>
 					заявку <br>
@@ -13,7 +13,7 @@
 				</p>
 			</div>
 			<div class="col-md-3">
-				<img src="img/zakaz.png" alt="img">
+				<div class="db-zakaz-img"><img src="img/zakaz.png" alt="img"></div>
 				<p class="db-zakaz-p"><span>ОФОРМЛЕНИЕ ЗАКАЗА</span><br>
 					Менеджер <br>
 					перезванивает <br>
@@ -21,14 +21,14 @@
 				</p>
 			</div>
 			<div class="col-md-3">
-				<img src="img/delivery.png" alt="img">
+				<div class="db-zakaz-img"><img src="img/delivery.png" alt="img"></div>
 				<p class="db-zakaz-p"><span>ДОСТАВКА</span><br>
 					Мы отправляем <br>
 					Ваш заказ <br>
 					в любой регион
 				</p>
 			</div><div class="col-md-3">
-			<img src="img/payment.png" alt="img">
+			<img src="img/payment.png" alt="img" class="db-zakaz-img">
 			<p class="db-zakaz-p"><span>ПОЛУЧЕНИЕ И ОПЛАТА</span><br>
 				Оплачиваете <br>
 				при получении
