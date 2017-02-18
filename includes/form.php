274bb3dd1b12ec094dee6d21c9d1b3f146b1f9ed
Barasbi <betokov93@gmail.com>
2017-02-18 17:02:07 +0300

    add 1

--- includes/form.php
+++ includes/form.php
@@ -1,20 +1,20 @@
 <section class="section section_padding section_db-form-view">
 	<div class=" container">
 		<div class="row">
-			
-			<form action="#" method="post">
-				<div class="col-md-4">
-					<input type="text" placeholder="Введите ваше имя" name="uname" class="input__view input_background1">
-				</div>
-				<div class="col-md-4">
-					<input type="text" placeholder="Введите ваш телефон" name="phone" class="input__view input_background2 js-mask">
-				</div>
-				<div class="col-md-4">
-					<input type="submit" name="submit" value="Заказать прямо сечас" class="button-1 db-form-button">
-				</div>
-				<div class="clearfix"></div>
-			</form>
-			
+			<div class="db-form-box">
+				<form action="#" method="post">
+					<div class="col-md-4">
+						<input type="text" placeholder="Введите ваше имя" name="uname" class="input__view input_background1">
+					</div>
+					<div class="col-md-4">
+						<input type="text" placeholder="Введите ваш телефон" name="phone" class="input__view input_background2 js-mask">
+					</div>
+					<div class="col-md-4">
+						<input type="submit" name="submit" value="Заказать прямо сечас" class="button-1 db-form-button">
+					</div>
+					<div class="clearfix"></div>
+				</form>
+			</div>
 		</div>
 	</div>
 </section>
