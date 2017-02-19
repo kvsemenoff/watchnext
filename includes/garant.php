56360c16c656d5a8b60fb93d5a544a4e1637dcea
Barasbi <betokov93@gmail.com>
2017-02-19 17:29:01 +0300

    add 1

--- includes/garant.php
+++ includes/garant.php
@@ -1,8 +1,23 @@
-<section class="section section_padding">
-<div class=" container">
-	<div class="row">
-		<div class="col-md-12">
+<section class="section section_padding section_db-garant-view">
+	<div class=" container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="db-zakaz-h2 db-garant-h2">гарантии</h2>
+			</div>
+			<div class="col-md-12">
+				<div class="db-garant-box-1">
+					<div class="db-garant-one">
+						<img src="img/prov.png" alt="img">
+					</div>
+					<div class="db-garant-two">
+						<p><span>100% проверка</span><br>
+							товара <br>
+							перед отправкой
+						</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
 		</div>
 	</div>
-</div>
 </section>
