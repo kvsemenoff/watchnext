a71ca20d1e15f43e5fae5ae31d97af0c28171180
ibragimtokmak <tokmakovibragim@gmail.com>
2017-02-19 13:06:34 +0300

    timer

---
+++ includes/timer.php
@@ -0,0 +1,25 @@
+<div class="dd-timer-all-wrap">
+    <div class="dd-timer">
+        <div class="dd-timer-title">
+            <span>до кончания акции осталось</span>
+        </div>
+        <div class="dd-times">
+            <div class="dd-div">
+                <span class="dd-hours dd-hours1"></span>
+                <span class="dd-hours dd-hours2"></span>
+                <br>
+                <span class="time-txt">Часов</span>
+            </div>
+            <div class="dd-div dd-div-margin">
+                <span class="dd-min dd-min1"></span>
+                <span class="dd-min dd-min2"></span><br>
+				<span class="time-txt">Минут</span>
+            </div>
+            <div class="dd-div dd-div-margin">
+                <span class="dd-sec dd-sec1"></span>
+                <span class="dd-sec dd-sec2"></span><br>
+				<span class="time-txt">Секунд</span>
+            </div>
+        </div>
+    </div>
+</div>
