90a9c9be259d288de9a538d8d1fde4becca76555
Barasbi <betokov93@gmail.com>
2017-02-19 19:59:13 +0300

    add 1

--- includes/garant.php
+++ includes/garant.php
@@ -17,7 +17,31 @@
 					</div>
 					<div class="clearfix"></div>
 				</div>
+				
+				<div class="db-garant-box-2">
+					<div class="db-garant-one">
+						<img src="img/law.png" alt="img">
+					</div>
+					<div class="db-garant-two">
+						<p><span>Соблюдение закона</span><br>
+							“О защите прав Потребителей”. В случае выявления <br>
+							заводского дефекта Вы вправе обменять товар на новый в течение<br> 14 дней с момента получения заказа. Пересылка будет<br> осуществляться за счёт магазина
+						</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+
+				<div class="db-garant-box-3">
+					<div class="db-garant-one">
+						<img src="img/garanty.png" alt="img">
+					</div>
+					<div class="db-garant-two">
+						<p><span>Гарантия доставки</span><br>
+							Все посылки отправляются наложным платежом. Это означает, что вы будете<br> платить за посылку на Каз почте при получении
+						</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
