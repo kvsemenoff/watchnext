7e8126821f6bd597db45817d6578442358b45217
Barasbi <web_masters_07@mail.ru>
2017-02-20 10:06:05 +0300

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<section class="section section_padding db-section-footer">
+	<div class=" container">
+		<div class="row">
+			<div class="col-md-12">
+				<p>ООО Торговый цех' ИНН 7801282818/ КПП 780101001 ОГРН 1157847209442 199106. г. Санкт-Петербург. 21я линия В.О.. дом 14 литера А. помещение 7-н <br>
+					Политика конфиденциальности
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
